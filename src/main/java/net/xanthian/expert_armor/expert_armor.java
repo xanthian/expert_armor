b35f758d7a225e584004d35a5b4cd95d485b71a3
xanthian <xanneh@hotmail.com>
2021-12-30 23:35:49 +0000

    1st

---
+++ src/main/java/net/xanthian/expert_armor/expert_armor.java
@@ -0,0 +1,27 @@
+package net.xanthian.expert_armor;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.item.ItemGroup;
+
+
+public class expert_armor implements ModInitializer {
+ 
+    // an instance of our new item
+    public static final Item DIAMOND_PLATE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Item IRON_PLATE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Item GOLD_PLATE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Item LEATHER_PLATE = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+ 
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("expert_armor", "diamond_plate"), DIAMOND_PLATE);
+		Registry.register(Registry.ITEM, new Identifier("expert_armor", "iron_plate"), IRON_PLATE);
+		Registry.register(Registry.ITEM, new Identifier("expert_armor", "gold_plate"), GOLD_PLATE);
+		Registry.register(Registry.ITEM, new Identifier("expert_armor", "leather_plate"), LEATHER_PLATE);
+    } 
+}
